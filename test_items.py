ed5c739b9c2c3e6e417c4acd3636cd1fec701126
Vika Denisenko <r.denysenko@flex-solution.com>
2022-07-07 20:02:16 +0300

    initial commit

---
+++ test_items.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+# команды для запуска с различными настройками
+# pytest -s -v --language=en test_items.py
+# pytest -s -v --language=ru test_items.py
+# pytest -s -v --language=es test_items.py
+# pytest -s -v --browser_name=firefox --language=fr test_items.py
+
+def test_button_is_on_page(browser):
+    browser.get(link)
+
+    button=browser.find_element(By.CLASS_NAME, "btn-primary")
+    print(button.text) # вывожу название кнопки в терминал и не использую time.sleep()
+
+    assert type(button) == WebElement, 'Кнопки купить нет на странице'
+
